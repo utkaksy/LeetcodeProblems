534d14751e68dba805c3eef723587e84a3e491b6
utkaksy <utkaksy01@hotmail.com>
2024-03-13 21:39:50 +0300

    Leetcode Daily Problem Solved

---
+++ 2485FindthePivotInteger.cs
@@ -0,0 +1,20 @@
+﻿public class Solution
+{
+    public int PivotInteger(int n)
+    {
+
+        int sol = 0, sag = 0;
+        for (int i = 1; i <= n; i++)
+        {
+
+            sol = (i * (i + 1)) / 2;
+            sag = ((n * (n + 1)) / 2) - sol + i;
+
+            if (sol == sag)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
