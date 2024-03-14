54fd0338fa4a774423d7e813a03696edc32f9332
utkaksy <utkaksy01@hotmail.com>
2024-03-14 17:40:03 +0300

    Time Complexity O(n2)

---
+++ 930BinarySubarraysWithSum.cs
@@ -0,0 +1,27 @@
+﻿public class Solution
+{
+    public int NumSubarraysWithSum(int[] nums, int goal)
+    {
+
+        int toplam = 0;
+        int output = 0;
+        //toplam=0
+        //output=4
+        //i=3
+        //j=3
+        for (int i = 0; i < nums.Length; i++)
+        {
+            for (int j = i; j < nums.Length; j++)
+            {
+                toplam = toplam + nums[j];
+                if (toplam == goal)
+                {
+                    output++;
+                }
+            }
+            toplam = 0;
+        }
+        return output;
+
+    }
+}
